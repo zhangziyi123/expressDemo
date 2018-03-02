ebc58b20ad9e60dd35e03e7684c4bdec463d35bb
zzy <492510851@qq.com>
2018-03-02 14:00:14 +0800

    优化sql封装

--- userms/routes/sql/users.js
+++ userms/routes/sql/users.js
@@ -4,71 +4,25 @@
  * @Date: 2018/2/28 22:53
  */
 var db = {};
-var pg = require('pg');
-
-var conString = "postgres://postgres:admin@localhost/userms";
-var client = new pg.Client(conString);
-
-db.query = function (sql, callback) {
+db.query = function (sql, client, callback) {
     var result = {};
     result.success = false;
-    client.connect(function (error, results) {
-        if (error) {
-            console.log('ClientConnectionReady Error: ' + error.message);
-            client.end();
-            return;
-        } else {
-            // var result = {};
-            console.log('connect success');
-            console.log(sql);
-            if (sql) {
-                client.query(sql, function (err, rows, fields) {
-                    if (err) {
-                        console.log('获取数据失败');
-                        client.end();
-                        return;
-                    }
-                    console.log('获取数据成功');
-                    result.success = true;
-                    result.rows = rows;
-                    result.fields = fields;
-                    client.end();
-                    callback&&callback(result);
-                })
-            } else {
-                result.success = false;
-                client.end();
-                callback&&callback(result);
+    if (sql) {
+        client.query(sql, function (err, dataInfo) {
+            if (err) {
+                console.log('获取数据失败');
+                return;
             }
-        }
-    })
-
-    /*  var result = {};
-     result.success = true;
-     if (sql) {
-     console.log('开始请求数据');
-     client.query(sql, function (err, rows, fields) {
-     console.log('获取数据成功');
-     result.success = true;
-     result.rows = rows;
-     result.fields = fields;
-     client.end();
-     })
-     } else {
-     result.success = false;
-     client.end();
-     }
-
-     return new Promise(function (resolve, reject) {
-     if (result.success) {
-     //成功操作,对应 then方法
-     resolve(result)
-     }
-     else {
-     //失败操作,对应catch方法
-     reject(result)
-     }
-     })*/
+            console.log('获取数据成功');
+            result.success = true;
+            result.rows = dataInfo.rows;
+            result.fields = dataInfo.fields;
+            callback && callback(result);
+        })
+    } else {
+        result.success = false;
+        callback && callback(result);
+    }
 }
 
 module.exports = db;
--- userms/routes/users.js
+++ userms/routes/users.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var sqlObj = require('./sql/connectSql.js');
 var db = require('./sql/users.js');
 /* GET users listing. */
 router.get('/user', function (req, res, next) {
@@ -11,20 +12,35 @@ router.get('/login', function (req, res, next) {
     var args = req.query;
     var name = req.query.name;
     var password = req.query.password;
-    /* console.log('get请求参数对象 :', req.query);
-     console.log('post请求参数对象 :', req.body);
-     console.log('q的值为 :', req.query.q);*/
-    if (name === 'zzy' && password === '123') {
-        // res.send('登录成功');
-    } else {
-        // res.send('登录失败');
-    }
-    db.query("select * from user_tb", function (result) {
-        res.send(result);
+    db.query("select * from user_tb", sqlObj, function (result) {
+        var flag = false;
+        var message = "";
+        if (result.success) {
+            for (var i = 0; i < result.rows.length; i++) {
+                if (result.rows[i].name.toString() === name && result.rows[i].password.toString() === password) {
+                    flag = true;
+                    break;
+                }
+
+                if (result.rows[i].name.toString() === name && result.rows[i].password.toString() !== password) {
+                    message = "密码错误!";
+                }
+            }
+        }
+        if (flag) {
+            res.send({success: true});
+        } else {
+            if (message === "") {
+                message = "用户名不存在!";
+            }
+            res.send({success: false, message: message});
+        }
     })
-    /*db.query("select * from user_tb").then(function (result) {
-     res.send(result);
-     })*/
 });
 
 module.exports = router;
+
+
+/* console.log('get请求参数对象 :', req.query);
+ console.log('post请求参数对象 :', req.body);
+ console.log('q的值为 :', req.query.q);*/
