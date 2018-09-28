f2265e44c249e858de7fe5580ebaadc977dd9755
zzy <492510851@qq.com>
2018-09-28 20:51:16 +0800

    11

---
+++ vueclient/src/components/user/threePoint.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>三点运算符</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: "子组件"
+    }
+  },
+  props: {
+    data: {
+      type: Object
+    },
+    name2: String,
+    getName: Function
+  },
+  mounted() {
+    debugger;
+    let ss = this;
+    let s2 = this.getName();
+    let s3 = this.name;
+    let tss = this.name2;
+    console.log(tss);
+  }
+}
+</script>
---
+++ vueclient/src/components/user/user.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>123131
+    <button @click="loginBtn">登录</button>
+  </div>
+</template>
+<script>
+import Vue from 'vue';
+import * as config from "./config";
+export default {
+  data () {
+    return {
+      msg: '',
+      password: "123132131",
+      toolTipMsg: "",
+      popperVM2: null,
+      name: "123"
+    }
+  },
+  mounted () {
+    debugger;
+    let ss = document.cookie;
+  },
+  methods: {
+    getNameFn(name) {
+      this.alertHello();
+      this.name = "lalala";
+      return "测试三点运算符传递方法";
+    },
+    alertHello() {
+    },
+    loginBtn() {
+    }
+  },
+  components: {
+  }
+}
+</script>
