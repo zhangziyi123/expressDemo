fb745a4ef78b123d00e8fc9abbb872f254007d99
zzy <492510851@qq.com>
2018-10-09 11:03:58 +0800

    解决sessionID每次请求问题

--- userms/app.js
+++ userms/app.js
@@ -4,11 +4,13 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session');
+var RedisStore = require('connect-redis')(session);  //定义redisStore
+
+var app = express();
 
 var index = require('./routes/index');
 var users = require('./routes/users');
-
-var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -20,8 +22,36 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(session({
+    // name: "sessionID", // 设置 cookie 中保存 session id 的字段名称
+    store: new RedisStore({
+        host: '127.0.0.1',		       //redis服务器地址, 这里用本机代替
+        port: 6379,              //自定端口
+        ttl: 1800 // 过期时间秒 Redis session TTL 过期时间 （秒）
+        //“pass”: redis_config.password  //如果有密码，就自己填进去
+    }),
+    resave: false, // don’t save session if unmodified
+    cookie: {
+        secure: false, // 是否每次请求都重新生成session的问题
+        maxAge: 1800000
+    },
+    saveUninitialized: true, // 是否将session存储到内存中。是否设置session在存储容器中可以给修改
+    secret: 'keyboard cat' // 通过设置 secret 来计算 hash 值并放在 cookie 中，使产生的 signedCookie 防篡改
+}));
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.all('*', function (req, res, next) {
+    /*res.header("Access-Control-Allow-Origin", "*");*/
+    res.header("Access-Control-Allow-Origin", "http://localhost:8086");
+    res.header("Access-Control-Allow-Credentials", "true"); // 处理cookie信息，如果有，并且不对每次请求都新开一个session
+    res.header("Access-Control-Allow-Headers", "X-Requested-With,token, autoken");
+    res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+    res.header("X-Powered-By", ' 3.2.1');
+    res.header("Cache-Control", 'no-store'); // 缓存
+    res.header("Content-Type", "application/json;charset=utf-8");
+    next();
+});
 app.use('/', index);
 app.use('/users', users);
 
@@ -42,5 +72,6 @@ app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.render('error');
 });
+
 
 module.exports = app;
--- userms/routes/users.js
+++ userms/routes/users.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 var sqlObj = require('./sql/connectSql.js');
 var db = require('./sql/users.js');
+var token = require('../public/util/token');
 /* GET users listing. */
-
 router.get('/user', function (req, res, next) {
     res.send('respond with a resource');
 });
@@ -14,6 +14,7 @@ router.get('/login', function (req, res, next) {
     var args = req.query;
     var name = req.query.name;
     var password = req.query.password;
+    console.log("login");
     // "select * from user_tb where name='zzy'",
     db.query("select * from user_tb", sqlObj, function (result) {
         var flag = false;
@@ -40,9 +41,38 @@ router.get('/login', function (req, res, next) {
         }
     })
 });
-
-router.get('/login/:name', function (req, res, next) {
-    res.send("hew1231123121231231zdfafasfasdf2");
+/*
+ * 保存用户token到session中 ？ 意义？ 用户每次登录的时候就读一次数据库，因为不知道数据库用户信息是否发生改变，这样操作不对吗？
+ * 用了token还有必要用session吗
+ * */
+router.get('/login/:name/:password', function (req, res, next) {
+    var params = {
+        name: req.params.name || '',
+        password: req.params.password || ''
+    }
+    if (params.name && params.password) {
+        var sqlStr = "select * from user_tb where name='" + params.name + "' and password = '" + params.password + "'";
+        db.query(sqlStr, sqlObj, function (result) {
+            if (result.success) {
+                if (result.rows.length > 0) {
+                    var data = result.rows[0];
+                    var userToken = token.createToken(data.userid);
+                    var userInfo = {
+                        name: data.name,
+                        userid: data.userid,
+                        token: userToken
+                    }
+                    req.session.userInfo = userInfo;
+                    res.send({success: true, userInfo: userInfo});
+                    // req.session.cookie.token = userToken; // 不能存cookie里面，不然再次请求获取不到值
+                } else {
+                    res.send({success: true, message: '用户名或密码错误!'});
+                }
+            } else {
+                res.send({success: false, message: 'server error'});
+            }
+        })
+    }
 });
 /* 修改密码接口 */
 router.get('/modify', function (req, res, next) {
@@ -55,7 +85,7 @@ router.get('/modify', function (req, res, next) {
 router.get('/modify/password', function (req, res, next) {
     console.log("修改用户信息接口");
     db.query("update user_tb set password='123' where name='zzy'", sqlObj, function (result) {
-        console.log(result);
+        console.log("modify", result);
         res.render('index', {title: 'Hey', message: 'Hello there!'});
         /*      if (result.success) {
          res.send({success: true, message: '修改密码成功!'});
@@ -64,6 +94,59 @@ router.get('/modify/password', function (req, res, next) {
          }*/
     })
 })
+
+/* 获取用户详情接口-根据token获取用户详情 */
+router.get('/userInfo', function (req, res, next) {
+    console.log("sessionID", req.sessionID);
+    console.log("sessionToken", req.session);
+    var tk = req.headers.autoken || "";
+    var userInfo = req.session.userInfo || "";
+    var sessionToken = userInfo ? userInfo.token : "";
+    console.log("tk", tk);
+    res.send({data: userInfo});
+    /*    if (tk && tk === sessionToken && token.checkToken(tk)) {
+     var userid = userInfo.userid;
+     var sqlStr = "select * from user_tb where userid = '" + userid + "'";
+     db.query(sqlStr, sqlObj, function (result) {
+     if (result.success) {
+     if (result.rows.length > 0) {
+     var data = result.rows[0];
+     res.send({success: true, userInfo: data});
+     } else {
+     res.send({success: true, message: "没有查询到该用户的详情"});
+     }
+     } else {
+     res.send({success: false, message: 'server error'});
+     }
+     })
+     } else {
+     res.send({success: false});
+     }*/
+})
+
+// 退出登录
+router.all('/logout', function (req, res, next) {
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Origin", req.headers.origin);
+    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    // 备注：这里用的 session-file-store 在destroy 方法里，并没有销毁cookie
+    // 所以客户端的 cookie 还是存在，导致的问题 --> 退出登陆后，服务端检测到cookie
+    // 然后去查找对应的 session 文件，报错
+    // session-file-store 本身的bug
+
+    req.session.destroy(function (err) {
+        if (err) {
+            res.json({ret_code: 2, ret_msg: '退出登录失败'});
+            return;
+        }
+        var identityKey = 'skey';
+        res.clearCookie(identityKey);
+        res.json(jsonData(0, null, '退出登录成功'));
+    });
+});
+
 module.exports = router;
 
 
